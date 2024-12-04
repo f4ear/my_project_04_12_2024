6645d0429ab5b22efed7debd96099fdb512806c3
f4ear Kopacz <szymonkopacz2@gmail.com>
2024-12-04 10:10:12 +0000

    added chat to rust

--- src/my_project_backend/src/lib.rs
+++ src/my_project_backend/src/lib.rs
@@ -1,19 +1,19 @@
 use std::cell::RefCell;
 
 thread_local! {
-    static MSG: RefCell<String> = RefCell::new(String::new());
+    static MSG: RefCell<Vec<String>> = RefCell::new(Vec::new());
 } 
 
 #[ic_cdk::update]
 fn set_msg(new_msg: String) {
-    MSG.with(|msg| {
-        *msg.borrow_mut() = new_msg;
+    CHAT.with(|chat| {
+        chat.borrow_mut().push(new_msg) 
     })
 }
 
 #[ic_cdk::query]
 fn get_msg() -> String {
-    MSG.with(|msg| msg.borrow().clone())
+    CHAT.with(|chat| chat.borrow().clone())
 }
 
 #[ic_cdk::query]
